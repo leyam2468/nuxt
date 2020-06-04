498c92ac6911f8428df97af46275eeaf583eafe6
leyam2468 <leyamm3@gmail.com>
2020-06-04 12:46:21 +0200

    4/6 12:46
    Signed-off-by: leyam2468 <leyamm3@gmail.com>

---
+++ components/navbar.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div class="nav">
+            <div class="container">
+                <nuxt-link to="/" class="nav-brand">Axel 4 Design</nuxt-link>
+                <div class="nav-bar">
+                    <nuxt-link to="/" class="nav-link">Home</nuxt-link>
+                    <nuxt-link to="/about" class="nav-link">About</nuxt-link>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+    width: 90%;
+    margin: auto;
+}
+a {
+    color: white;
+    text-decoration: none;
+    display: inline-block;
+}
+
+.nav {
+    background-color: black;
+    color: white;
+    padding: 10px;
+
+    margin: auto;
+}
+.nav-brand {
+    font-size: 30px;
+    font-weight: bold;
+    display: inline-block;
+}
+.nav-bar {
+    display: inline-block;
+    float: right;
+}
+.nav-link {
+    font-size: 25px;
+    font-weight: bold;
+    margin: 0 10px;
+    transition: 0.5s;
+}
+.nav-link:hover {
+    color: red;
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Welcome To About Page</h1>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+h1 {
+    font-size: 5rem;
+    margin: auto;
+}
+</style>
